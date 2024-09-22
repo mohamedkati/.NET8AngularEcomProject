72adc6da88825837f13e4c4276fcffc49b83ca15
Mohammed Kati <mohammed.kati@Axeciel.fr>
2024-09-22 12:16:20 +0100

    implement sort, paginate, filter data  and create the client app

---
+++ Client/src/app/app.module.ts
@@ -0,0 +1,4 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
